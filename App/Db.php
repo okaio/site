fd8fe707b07e54cc2aa25ef0e46e9a35cdfe40cc
Sidorov Alexandr <taygareason@gmail.com>
2017-07-30 17:54:18 +0300

    написан класс Db

--- App/Db.php
+++ App/Db.php
@@ -4,7 +4,27 @@ namespace App;
 
 class Db {
 
+    const HOST = 'mysql:host=localhost;dbname=test;charset=UTF8;';
+    const NAME = 'root';
+    const PASS = '1613';
+    protected $dbh;
+
+
     public function __construct () {
-        echo 'hello';
+        $this->dbh = new \PDO(self::HOST, self::NAME, self::PASS);
+    }
+
+    public function execute ($sql) {
+        $sth=$this->dbh->prepare($sql);
+        $res=$sth->execute();
+        return $res;
+    }
+    public function query ($sql) {
+        $sth=$this->dbh->prepare($sql);
+        $res=$sth->execute();
+        if(false!== $res) {
+            return $sth->fetchAll();
+        }
+        return [];
     }
 }
